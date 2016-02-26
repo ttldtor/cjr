9ef7b76eaaa8273b91fe2e47e9dbc815a0593ce9
ttldtor <ttldtor@gmail.com>
2016-02-27 01:07:52 +0300

    menu bar

--- src/main/kotlin/ttldtor/App.kt
+++ src/main/kotlin/ttldtor/App.kt
@@ -1,15 +1,39 @@
 package ttldtor
 
 import javafx.application.Application;
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
 import javafx.scene.Scene
-import javafx.scene.layout.StackPane
+import javafx.scene.control.Menu
+import javafx.scene.control.MenuBar
+import javafx.scene.control.MenuItem
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
+
+fun createMenuBar(): MenuBar {
+    val menuBar = MenuBar()
+    val menuFile = Menu("File")
+
+    val exitItem = MenuItem("Exit")
+
+    exitItem.onAction = EventHandler<ActionEvent> {
+        a -> System.exit(0)
+    }
+
+    menuFile.items.addAll(exitItem)
+
+    menuBar.menus.addAll(menuFile, Menu("Edit"))
+
+    return menuBar
+}
 
 class MainGui: Application() {
     override fun start(stage: Stage) {
         stage.title = "Test";
 
-        val pane = StackPane()
+        val pane = VBox()
+
+        pane.children.addAll(createMenuBar())
 
         stage.scene = Scene(pane, 800.0, 600.0)
         stage.show()
