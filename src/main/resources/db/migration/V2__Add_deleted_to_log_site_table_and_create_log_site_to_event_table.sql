bfc5c9cc2a588265bd2c2413c9cc7b513c4d32ca
ttldtor <ttldtor@gmail.com>
2016-05-31 01:41:46 +0300

    Log Site full CRUD + UI #5: + log_site.deleted column event_event_type_id -> event_event_type_id_fk + log_site_to_event table

---
+++ src/main/resources/db/migration/V2__Add_deleted_to_log_site_table_and_create_log_site_to_event_table.sql
@@ -0,0 +1,13 @@
+ALTER TABLE log_site
+ADD IF NOT EXISTS deleted BOOLEAN DEFAULT FALSE;
+
+ALTER TABLE event RENAME event_event_type_id TO event_event_type_id_fk;
+
+CREATE TABLE log_site_to_event (
+    id IDENTITY PRIMARY KEY NOT NULL,
+    log_site_id bigint NOT NULL,
+    event_id bigint NOT NULL,
+    CONSTRAINT log_site_to_event_log_site_id_fk FOREIGN KEY (log_site_id) REFERENCES log_site (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT log_site_to_event_event_id_fk FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
