ff92fdc307174ea276c73cf814fddcc29dc1091d
ttldtor <ttldtor@gmail.com>
2016-06-14 22:06:54 +0300

    Log Site full CRUD + UI #5: the Save method

--- src/main/kotlin/ttldtor/dao/LogSiteDao.kt
+++ src/main/kotlin/ttldtor/dao/LogSiteDao.kt
@@ -7,11 +7,12 @@ import ttldtor.use
 import java.sql.Statement
 
 object LogSiteDao {
-    val INSERT_SQL = "INSERT INTO log_site (name, conference, url, last_parsed_timestamp) VALUES (?, ?, ?, ?)"
+    val CREATE_SQL = "INSERT INTO log_site (name, conference, url, last_parsed_timestamp) VALUES (?, ?, ?, ?)"
+    val SAVE_SQL = "UPDATE log_site SET name = ?, conference = ?, url = ?, last_parsed_timestamp = ? WHERE id = ?"
 
     fun create(name: String, conference: String, url: String, lastParsedDate: Date): LogSite? {
-        return ConnectionPool.connection.use {conn ->
-            conn.prepareStatement(INSERT_SQL, Statement.RETURN_GENERATED_KEYS).use ust@ {st ->
+        return ConnectionPool.connection.use { conn ->
+            conn.prepareStatement(CREATE_SQL, Statement.RETURN_GENERATED_KEYS).use ust@ { st ->
                 st.setString(1, name)
                 st.setString(2, conference)
                 st.setString(3, url)
@@ -34,5 +35,19 @@ object LogSiteDao {
 
     fun create(logSite: LogSite): LogSite? {
         return create(logSite.name, logSite.conference, logSite.url, logSite.lastParsedDate)
+    }
+
+    fun save(logSite: LogSite): Boolean {
+        return ConnectionPool.connection.use { conn ->
+            conn.prepareStatement(SAVE_SQL).use ust@ { st ->
+                st.setString(1, logSite.name)
+                st.setString(2, logSite.conference)
+                st.setString(3, logSite.url)
+                st.setLong(4, logSite.lastParsedDate.time)
+                st.setLong(5, logSite.id)
+
+                return@ust st.executeUpdate() == 1
+            }
+        }
     }
 }
